6a82376db817fd3219000632f0964fef64a17398
OG <omargomez2@gmail.com>
2020-01-15 21:24:45 -0500

    v4

--- restapp/src/main/resources/data.sql
+++ restapp/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 INSERT INTO 
     oil_production (month, production) 
 VALUES
-    ('january', 524000),
-    ('february', 533000),
-    ('march', 530000),
-    ('april', 529000),
-    ('may', 532000),
-    ('june', 531000),
-    ('july', 541000),
-    ('august', 550000),
-    ('september', 547000),
-    ('october', 467000),
-    ('november', 546000);
+    ('january19', 524000),
+    ('february19', 533000),
+    ('march19', 530000),
+    ('april19', 529000),
+    ('may19', 532000),
+    ('june19', 531000),
+    ('july19', 541000),
+    ('august19', 550000),
+    ('september19', 547000),
+    ('october19', 467000),
+    ('november19', 546000);
